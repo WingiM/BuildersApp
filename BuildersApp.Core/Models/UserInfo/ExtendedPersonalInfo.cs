07784062edde5fc18f943b2a9d5b002f41e397dd
Kamil Sharipov <wingimobile@gmail.com>
2022-11-15 08:02:38 +0300

    fix bugs

--- BuildersApp.Core/Models/UserInfo/ExtendedPersonalInfo.cs
+++ BuildersApp.Core/Models/UserInfo/ExtendedPersonalInfo.cs
@@ -6,12 +6,15 @@ namespace BuildersApp.Core.Models.UserInfo;
 public abstract class ExtendedPersonalInfo : PersonalInfoBase
 {
     [StringLength(maximumLength: 13, MinimumLength = 13, ErrorMessage = "Must be 13 symbols long")]
+    [Phone(ErrorMessage = "Must be number")]
     public string Ogrn { get; set; }
 
     [StringLength(maximumLength: 10, MinimumLength = 10, ErrorMessage = "Must be 10 symbols long")]
+    [Phone(ErrorMessage = "Must be number")]
     public string Tin { get; set; }
 
     [StringLength(maximumLength:9, MinimumLength = 9, ErrorMessage = "Must be 9 symbols long")]
+    [Phone(ErrorMessage = "Must be number")]
     public string Kpp { get; set; }
     public string Address { get; set; }
 }
